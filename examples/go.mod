ad6b8d3e2e84b830aaa4f6af5d4d2d5a9f5d3d94
CaptainNEO <tianpengfei@rcrai.com>
2022-05-25 16:00:10 +0800

    add examples

--- examples/go.mod
+++ examples/go.mod
@@ -2,10 +2,11 @@ module examples
 
 go 1.18
 
-require github.com/gofiber/fiber/v2 v2.31.0
+require github.com/gofiber/fiber/v2 v2.34.0
 
-require github.com/captain-neo/soda v0.0.13
-replace github.com/captain-neo/soda v0.0.13 => ../
+require github.com/captain-neo/soda v0.0.27
+
+replace github.com/captain-neo/soda v0.0.27 => ../
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
@@ -15,17 +16,17 @@ require (
 	github.com/go-openapi/swag v0.21.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.10.1 // indirect
+	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/gorilla/schema v1.2.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/klauspost/compress v1.15.1 // indirect
+	github.com/klauspost/compress v1.15.4 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.35.0 // indirect
+	github.com/valyala/fasthttp v1.37.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29 // indirect
-	golang.org/x/sys v0.0.0-20220406163625-3f8b81556e12 // indirect
+	golang.org/x/crypto v0.0.0-20220518034528-6f7dac969898 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- examples/main.go
+++ examples/main.go
@@ -6,42 +6,47 @@ import (
 	"github.com/captain-neo/soda"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/fiber/v2/middleware/monitor"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
-
-type ExampleRequestBody struct {
-	String      string
-	StringSlice []string
-	Int         int `json:"int"`
-}
 
 type Auth struct {
 	Token string `header:"Authorization" oai:"description=some JWT Token"`
 }
 
-type ExampleParameters struct {
+type ExParameters struct {
 	Auth
-	Q      []string `query:"q" oai:"description=support list parameters"`
-	Limit  int      `query:"limit" oai:"description=blabla"`
-	Offset int      `query:"offset"`
+	ID     int      `json:"id"     validate:"required" oai:"description=some id"                 path:"id"`
+	Q      []string `json:"q"      validate:"required" oai:"description=support list parameters"           query:"q"`
+	Limit  int      `json:"limit"  validate:"required" oai:"description=limit params"                      query:"limit"`
+	Offset int8     `json:"offset" validate:"required" oai:"description=offset params"                     query:"offset"`
+	// Cookie string   `validate:"required" oai:"description=i am the cookie"                                  cookie:"cookie"`
+}
+
+type ExBody struct {
+	ID     int      `json:"id"     validate:"required" oai:"description=我是结构体里面的ID"`
+	Q      []string `json:"q"      validate:"required" oai:"description=这是一个支持模糊查询的字符串列表"`
+	Limit  int      `json:"limit"  validate:"required" oai:"description=limit;maximum=20"`
+	Offset int      `json:"offset" validate:"required" oai:"description=offset"`
 }
 
 type ExampleResponse struct {
-	Parameters  *ExampleParameters  `json:"parameters"`
-	RequestBody *ExampleRequestBody `json:"request_body"`
+	Parameters  *ExParameters `json:"parameters"`
+	RequestBody *ExBody       `json:"request_body"`
 }
 type ErrorResponse struct{}
 
-func exampleHandler(c *fiber.Ctx) error {
-	// get parameter values
-	params := c.Locals(soda.KeyParameter).(*ExampleParameters)
+func exampleGet(c *fiber.Ctx) error {
+	params := c.Locals(soda.KeyParameter).(*ExParameters)
 	fmt.Println(params.Token, params.Limit, params.Offset, params.Q)
-	// get request body values
-	body := c.Locals(soda.KeyRequestBody).(*ExampleRequestBody)
-	fmt.Println(body.Int)
 	return c.Status(200).JSON(ExampleResponse{
-		Parameters:  params,
+		Parameters: params,
+	})
+}
+
+func examplePost(c *fiber.Ctx) error {
+	body := c.Locals(soda.KeyRequestBody).(*ExBody)
+	fmt.Println(body.Limit, body.Offset, body.Q)
+	return c.Status(200).JSON(ExampleResponse{
 		RequestBody: body,
 	})
 }
@@ -55,13 +60,14 @@ func main() {
 		soda.EnableValidateRequest(),
 	)
 	app.Use(logger.New(), requestid.New())
-	app.Get("/monitor", monitor.New()).SetSummary("it's a monitor").OK()
-	app.Post("/path", exampleHandler).
-		SetOperationID("example-handler").
-		SetJSONRequestBody(ExampleResponse{}).
-		SetParameters(ExampleParameters{}).
+	app.Get("/test/:id", exampleGet).
+		SetParameters(ExParameters{}).
 		AddJSONResponse(200, ExampleResponse{}).
 		AddJSONResponse(400, ErrorResponse{}).OK()
 
+	app.Post("/test", examplePost).
+		SetJSONRequestBody(ExBody{}).
+		AddJSONResponse(200, ExampleResponse{}).
+		AddJSONResponse(400, ErrorResponse{}).OK()
 	_ = app.Listen(":8080")
 }
